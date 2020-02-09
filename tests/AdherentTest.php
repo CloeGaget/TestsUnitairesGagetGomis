f886627bdc7b8b1f4f5d4868de17657540ba58a9
LucasLPRO <lucas38gomis@gmail.com>
2020-02-09 18:46:06 +0100

    Test sur la methode getIdentifiantNormalise

--- tests/AdherentTest.php
+++ tests/AdherentTest.php
@@ -6,7 +6,18 @@ use PHPUnit\Framework\TestCase;
 final class AdherentTest extends TestCase {
     public function testCanBeCreated(): void {
         $adherent = new Adherent("michmich", "delacompta", "2019-12-12");
-        $this->assertInstanceOf(Adherent::Class,$adherent);
+        $this->assertInstanceOf(Adherent::Class, $adherent);
+    }
+    
+    public function testIsIdentifiantNormalise(): void {
+        
+    }
+    
+    public function testCreateName(): void {
+        $adherent = new Adherent('gomis', 'lucas', '01/01/1995');
+        $this->assertEquals('GOMISLUCAS01011995',
+            $adherent->getIdentifiantNormalise()
+        );
     }
 }
 
