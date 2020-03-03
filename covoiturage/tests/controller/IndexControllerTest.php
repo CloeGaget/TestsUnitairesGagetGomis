c794951353956e46d6d38a648400f95f6543f1e1
LucasLPRO <lucas38gomis@gmail.com>
2020-03-03 15:17:13 +0100

    test indexController

--- covoiturage/tests/controller/IndexControllerTest.php
+++ covoiturage/tests/controller/IndexControllerTest.php
@@ -1,9 +1,53 @@
 <?php
 namespace App\Tests\Entity;
 
-use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\WebTestCase;
 
-class IndexControllerTest extends TestCase {
-    
-    
+class IndexControllerTest extends WebTestCase {
+    public function testHomepage()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+    }
+
+    public function testHomepageLinkConnexion()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Connexion')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/login', $client->getRequest()->getPathInfo());
+    }
+
+    public function testHomepageLinkRegister()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Sign up')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/register', $client->getRequest()->getPathInfo());
+    }
+
+    public function testHomepageLinkResearchTrajet()
+    {
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+
+        $link = $crawler->selectLink('Rechercher trajet')->link();
+        
+        $client->click($link);
+
+        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals('/research-trajet', $client->getRequest()->getPathInfo());
+    }
 }
