37b3c0f8040e1fa4e8cd26ad0371235bc86416f5
CloeGaget <57403315+CloeGaget@users.noreply.github.com>
2020-03-03 14:12:07 +0100

    koukou

--- covoiturage/src/Controller/IndexController.php
+++ covoiturage/src/Controller/IndexController.php
@@ -3,12 +3,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use App\Service\PanierService;
 
-class DefaultController extends AbstractController {
+class IndexController extends AbstractController {
     public function index() {
-        return $this->render('index.html.twig');
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
     }
 }
-
 
 
 ?>
