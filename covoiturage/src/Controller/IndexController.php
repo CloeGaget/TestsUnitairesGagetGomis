e1ee1175b496e4134c004dac0e11e80a3b0bccd5
LucasLPRO <lucas38gomis@gmail.com>
2020-03-03 13:54:08 +0100

    test security controller

--- covoiturage/src/Controller/IndexController.php
+++ covoiturage/src/Controller/IndexController.php
@@ -1,7 +1,6 @@
 <?php
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use App\Service\PanierService;
 
 class DefaultController extends AbstractController {
     public function index() {
